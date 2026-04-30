39b6901722f65794c68e9731607bea143486dbd6
shuftipro <support@shuftipro.com>
2026-04-30 17:42:16 +0500

    Update Package.swift with release download URL

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ShuftiPro",
-            url: "https://github.com/shuftipro/testframework/raw/1.0.0/ShuftiPro.xcframework.zip",
+            url: "https://github.com/shuftipro/testframework/releases/download/1.0.0/ShuftiPro.xcframework.zip",
             checksum: "7d27ccf995ed457c1ac22ec7aebac87ad477450d06da6bbc7808c77141d334f2"
         )
     ]
